d0b4d02e966e80033ca86ff9f3c3afd7b711c5d7
Ri7ay <st060614@student.spbu.com>
2021-05-22 20:36:41 +0300

    add ci_string task

---
+++ src/ci_string.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <cstring>
+#include <cassert>
+
+struct ci_char_traits : public std::char_traits<char> {
+    static bool eq(char lhs, char rhs) {
+        return std::tolower(lhs) == std::tolower(rhs);
+    }
+
+    static bool lt(char lhs, char rhs) {
+        return std::tolower(lhs) < std::tolower(rhs);
+    }
+
+    static int compare(const char *s1,
+                       const char *s2,
+                       size_t n) {
+        // return std::memicmp(s1, s2, n); for g++ compiler
+        while ((*s1 != '\0' || *s2 == '\0') && n-- > 0) {
+            if (std::tolower(*s1) > std::tolower(*s2)) {
+                return 1;
+            } else if (std::tolower(*s1) < std::tolower(*s2)) {
+                return -1;
+            }
+            ++s1;
+            ++s2;
+        }
+        return 0;
+    }
+
+    static const char *find(const char *s, int n, char a) {
+        while (n-- > 0 && toupper(*s) != toupper(a)) {
+            ++s;
+        }
+        return n >= 0 ? s : nullptr;
+    }
+};
+
+typedef std::basic_string<char, ci_char_traits> ci_string;
+
+int main() {
+    ci_string s("AbCdE");
+    // Нечувствительно к регистру
+    assert(s == "abcde");
+    assert(s == "ABCDE");
+    // Остается чувствительно к регистру
+    assert(strcmp(s.c_str(), "AbCdE") == 0);
+    assert(strcmp(s.c_str(), "abcde") != 0);
+    return 0;
+}
